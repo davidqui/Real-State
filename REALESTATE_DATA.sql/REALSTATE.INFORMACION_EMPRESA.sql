db78ebaa0df8c91e750cae3f2d24801c4b335799
davidqui <davidquijanoramos@gmail.com>
2018-02-11 16:22:55 -0500

    ejercicio inicial completo

---
+++ REALESTATE_DATA.sql/REALSTATE.INFORMACION_EMPRESA.sql
@@ -0,0 +1,8 @@
+SET DEFINE OFF;
+Insert into REALDB.INFORMACION_EMPRESA
+   (NIT, DIRE_EMP, TEL_EMP, CELULAR, EMAL, 
+    FAX, URL, ABOUT, REQUEST)
+ Values
+   ('800999999                     ', 'CALLE 26 # 52 - 00', '234213', '234688', 'info@real.com', 
+    '2345676', 'www.REALDB.com', 'somos una empresa de compra,venta,arriendo por internet', 'en el 2025 contratrar al grupo getde para que realice todo el diseño y desarrollo de nuestro ERP');
+COMMIT;
---
+++ REALESTATE_DATA.sql/REALSTATE.USUARIOS.sql
@@ -0,0 +1,32 @@
+SET DEFINE OFF;
+Insert into REALDB.USUARIOS
+   (ID_USER, CLI_USER, NOMBRES, APELLIDOS, DIR_U, 
+    TELE_U, CEL_U, CORREO_U, PAIS, PASS_U)
+ Values
+   (14637425, 1, 'carlos andres', 'velasco zuluaga', 'calle 10 # 11-15', 
+    '3108418679', '3108418679', 'carvelzu@gmail.com', 'COLOMBIA', '12345');
+Insert into REALDB.USUARIOS
+   (ID_USER, CLI_USER, NOMBRES, APELLIDOS, DIR_U, 
+    TELE_U, CEL_U, CORREO_U, PAIS, PASS_U)
+ Values
+   (77180615, 1, 'Ciro Edgardo', 'Mora Armesto', 'La piscina', 
+    '123456', '123456', 'moramora1979@hotmail.com', 'COLOMBIA', '12345');
+Insert into REALDB.USUARIOS
+   (ID_USER, CLI_USER, NOMBRES, APELLIDOS, DIR_U, 
+    TELE_U, CEL_U, CORREO_U, PAIS, PASS_U)
+ Values
+   (10122320941, 1, 'Diego Armando', 'Leal Calderon', 'El castillo', 
+    '23456', '23456', 'diegoar860510@hotmail.com', 'COLOMBIA', '123456');
+Insert into REALDB.USUARIOS
+   (ID_USER, CLI_USER, NOMBRES, APELLIDOS, DIR_U, 
+    TELE_U, CEL_U, CORREO_U, PAIS, PASS_U)
+ Values
+   (9770253, 1, 'JohanMiguel', 'Cespedes Ortea', 'Prepagos VIP', 
+    '234567', '34567', 'johan6821@hotmail.com', 'COLOMBIA', '12345');
+Insert into REALDB.USUARIOS
+   (ID_USER, CLI_USER, NOMBRES, APELLIDOS, DIR_U, 
+    TELE_U, CEL_U, CORREO_U, PAIS, PASS_U)
+ Values
+   (6844197, 2, 'deybys', 'Muñoz Ramos', 'Calle 100 # 12-14', 
+    '234567', '234567', 'demurg1960@hotmail.com', 'COLOMBIA', '234567');
+COMMIT;
